a2cd3dc8c1d3125fdfa3429a9d3c8d48e12ccbc0
Fondry <46901096+Fondry@users.noreply.github.com>
2019-03-01 11:33:43 +0100

    Update ternlabel.m
    Hi! first of all, thank you for this amazing ternary diagram creator, i`m a chemical engineering student and this is awesome. Nevertheless, I have a little concern about the label position.
    The text are too close to the numbers of the diagram and it made mine look like this: https://pasteboard.co/I3oawRe.png
    So it might be better to change the numbers a little, for me the following numbers worked better: https://pasteboard.co/I3ocsgQ.png
    I´m just a begginer to programming, but i hope it will be useful.

--- ternlabel.m
+++ ternlabel.m
@@ -13,11 +13,11 @@
 % Modifications
 
 % Modifiers
-
-function h = ternlabel(A, B, C)
-r(1) = text(0.5, -0.05, A, 'horizontalalignment', 'center');
-r(2) = text(1-0.45*sin(deg2rad(30)), 0.5, B, 'rotation', -60, 'horizontalalignment', 'center');
-r(3) = text(0.45*sin(deg2rad(30)), 0.5, C, 'rotation', 60, 'horizontalalignment', 'center');
+                                    
+function h = ternlabel(A, B, C)                                      
+r(1) = text(0.5, -0.08, A, 'horizontalalignment', 'center');  
+r(2) = text(1-0.30*sin(deg2rad(30)), 0.5, B, 'rotation', -60, 'horizontalalignment', 'center');
+r(3) = text(0.30*sin(deg2rad(30)), 0.5, C, 'rotation', 60, 'horizontalalignment', 'center');
 
 if nargout > 0
     h = r;
